30206bc11190bb8776ad3540878eeaafcb5b7da8
Paul Mackay <paul.m.mackay85@gmail.com>
2021-03-04 17:02:47 +0000

    Adding Items to the Cart

---
+++ models/cart-item.js
@@ -0,0 +1,12 @@
+import { ThemeColors } from "react-navigation";
+
+class CartItem {
+    constructor(quantity, productPrice, productTitle, sum) {
+        this.quantity = quantity;
+        this.productPrice = productPrice;
+        this.productTitle = productTitle;
+        this.sum = sum;
+    }
+}
+
+export default CartItem;
