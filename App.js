240960cc167a8479f6a98959b9b8b70a248a3e78
Paul Mackay <paul.m.mackay85@gmail.com>
2021-03-08 16:22:41 +0000

    still getting errors from view orders button

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import AppLoading from 'expo-app-loading';
+import { enableScreens } from 'react-native-screens';
 import * as Font from 'expo-font';
 
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/orders';
 import ShopNavigator from './navigation/ShopNavigator';
+
+enableScreens();
 
 const rootReducer = combineReducers({
   products: productsReducer,
