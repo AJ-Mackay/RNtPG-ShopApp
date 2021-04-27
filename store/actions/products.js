8758741934818234617e92c27442cf2c8f17c4a4
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-27 13:57:05 +0100

    Displaying a Loading Spinner & Handling Errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,27 +7,37 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
     return async dispatch => {
-        const response = await fetch(
-            'https://rntpg-shopapp-default-rtdb.firebaseio.com/products.json'
-        );
+        // any async code you want
+        try {
+            const response = await fetch(
+                'https://rntpg-shopapp-default-rtdb.firebaseio.com/products.json'
+            );
 
-        const resData = await response.json();
-        const loadedProducts = [];
-
-            for (const key in resData) {
-                loadedProducts.push(
-                    new Product(
-                        key,
-                        'u1',
-                        resData[key].title,
-                        resData[key].imageUrl,
-                        resData[key].description,
-                        resData[key].price
-                    )
-                );
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
             }
 
-        dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+            const resData = await response.json();
+            const loadedProducts = [];
+
+                for (const key in resData) {
+                    loadedProducts.push(
+                        new Product(
+                            key,
+                            'u1',
+                            resData[key].title,
+                            resData[key].imageUrl,
+                            resData[key].description,
+                            resData[key].price
+                        )
+                    );
+                }
+
+            dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+        } catch (err) {
+            // send to custom analytics server
+            throw err;
+        }
     };
 };
 
