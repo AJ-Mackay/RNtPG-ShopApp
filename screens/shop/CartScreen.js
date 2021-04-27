7035cebaab7666eef1903e71cf2bac6eb5268e5c
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-27 14:53:29 +0100

    Displaying an ActivityIndicator

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, StyleSheet, Button, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Color from '../../constants/Colors';
@@ -7,8 +7,10 @@ import CartItem from '../../components/shop/CartItem';
 import Card from '../../components/UI/Card';
 import * as cartActions from '../../store/actions/cart';
 import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const CartScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
     const cartItems = useSelector(state => {
         const transformedCartItems = [];
@@ -27,20 +29,28 @@ const CartScreen = props => {
     });
     const dispatch = useDispatch();
 
+    const sendOrderHandler = async () => {
+        setIsLoading(true);
+        await dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
+        setIsLoading(false);
+    }
+
     return ( 
         <View style={styles.screen}>
             <Card style={styles.summary}>
                 <Text style={styles.summaryText}>
                     Total: <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
                 </Text>
-                <Button 
-                    color={Color.accent}
-                    title='Order Now'
-                    disabled={cartItems.length === 0}
-                    onPress={() => {
-                        dispatch(ordersActions.addOrder(cartItems, cartTotalAmount));
-                    }}
-                />
+                {isLoading ? (
+                    <ActivityIndicator size='small' color={Colors.primary} />
+                    ) : (
+                    <Button 
+                        color={Color.accent}
+                        title='Order Now'
+                        disabled={cartItems.length === 0}
+                        onPress={sendOrderHandler}
+                    />
+                )}
             </Card>
             <FlatList
                 data={cartItems}
