74e3044215f9fe5421624fd6237c0d3d481e9521
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-27 14:36:01 +0100

    Handling Additional Errors

--- store/actions/products.js
+++ store/actions/products.js
@@ -43,9 +43,14 @@ export const fetchProducts = () => {
 
 export const deleteProduct = productId => {
     return async dispatch => {
-        await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/products/${productId}.json`, {
+        const response = await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/products/${productId}.json`, {
             method: 'DELETE'
         });
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
+        
         dispatch({ type: DELETE_PRODUCT, pid: productId });
     };
 };
@@ -85,8 +90,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
     return async dispatch => {
-
-        await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/products/${id}.json`, {
+        const response = await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/products/${id}.json`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json'
@@ -97,6 +101,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
                 imageUrl
             })
         });
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!');
+        }
 
         dispatch({
             type: UPDATE_PRODUCT,
