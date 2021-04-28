19afe7f5a572704d01368642b5bf3f7917988df3
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-28 15:46:30 +0100

    Mapping Orders to Users

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -5,9 +5,10 @@ export const SET_ORDERS = 'SET_ORDERS';
 
 export const fetchOrders = () => {
     try {
-        return async dispatch => {
+        return async (dispatch, getState) => {
+            const userId = getState().auth.userId;
             const response = await fetch(
-                'https://rntpg-shopapp-default-rtdb.firebaseio.com/orders/u1.json'
+                `https://rntpg-shopapp-default-rtdb.firebaseio.com/orders/${userId}.json`
             );
 
             if (!response.ok) {
@@ -37,8 +38,9 @@ export const fetchOrders = () => {
 export const addOrder = (cartItems, totalAmount) => {
     return async (dispatch, getState) => {
         const token = getState().auth.token;
+        const userId = getState().auth.userId;
         const date = new Date();
-        const response = await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/orders/u1.json?auth=${token}`, {
+        const response = await fetch(`https://rntpg-shopapp-default-rtdb.firebaseio.com/orders/${userId}.json?auth=${token}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
