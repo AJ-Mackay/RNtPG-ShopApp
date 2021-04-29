878c3781eecc7162d02d012c6b4b75f8a2d4a95b
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-29 16:22:25 +0100

    Implementing 'Auto Login' & fixing API Key

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,12 +1,17 @@
-export const SIGNUP = 'SIGNUP';
-export const LOGIN = 'LOGIN';
+import { useAsyncStorage } from '@react-native-community/async-storage';
 
-const API_KEY = process.env.API_KEY;
+export const AUTHENTICATE = 'AUTHENTICATE';
+
+export const authenticate = (userId, token) => {
+    return { type: AUTHENTICATE, userId: userId, token: token };
+};
+
+// API Key removed from live database and new one regenerated (29/4/21)
 
 export const signup = (email, password) => {
     return async dispatch => {
         const response = await fetch(
-            `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, 
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAGAhbRq3VkWA7X1SGh9fCN1lzV5kKJnDk', 
             {
                 method: 'POST',
                 headers: {
@@ -32,14 +37,16 @@ export const signup = (email, password) => {
 
     const resData = await response.json();
     console.log(resData);
-    dispatch({ type: SIGNUP, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.idToken, resData.localId));
+    const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
     };
 };
 
 export const login = (email, password) => {
     return async dispatch => {
         const response = await fetch(
-            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`, 
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAGAhbRq3VkWA7X1SGh9fCN1lzV5kKJnDk', 
             {
                 method: 'POST',
                 headers: {
@@ -67,6 +74,16 @@ export const login = (email, password) => {
 
     const resData = await response.json();
     console.log(resData);
-    dispatch({ type: LOGIN, token: resData.idToken, userId: resData.localId });
+    dispatch(authenticate(resData.idToken, resData.localId));
+    const expirationDate = new Date(new Date().getTime() + parseInt(resData.expiresIn) * 1000);
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
     };
+};
+
+const saveDataToStorage = (token, userId, expirationDate) => {
+    useAsyncStorage.setItem('userData', JSON.stringify({
+        token: token,
+        userId: userId,
+        expiryDate: expirationDate.toISOString()
+    }));
 };
